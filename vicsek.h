9cf5277e315e14ca928b82f6f69820ba1d386ce2
Jules <151455192+TheLuge0@users.noreply.github.com>
2026-09-25 18:34:38 +0200

    Update vicsek.h

--- vicsek.h
+++ vicsek.h
@@ -2,11 +2,11 @@
 #define CONSTANTS_H
 
 #define WINDOW_WIDTH 1300 
-#define BORDER_WIDTH 400 // 100
+#define BORDER_WIDTH 400 
 #define WINDOW_HEIGHT 730 
-#define BORDER_HEIGHT 200 // 100
+#define BORDER_HEIGHT 200
 
-#define VECTOR_SIZE 0.2
+#define VECTOR_SIZE 0.2 
 #define ARROW_SIZE 5
 
 #define FISH_WIDTH 1
@@ -14,7 +14,7 @@
 #define FISH_SPEED 150
 #define FISH_NUMBER 500
 
-#define INFLUENCE_RADIUS 150 // 80
+#define INFLUENCE_RADIUS 150 
 #define NOISE 1.275
 #define DELTA_TIME 1
 #define DT 0.03
